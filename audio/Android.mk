e0e8559a7041932f9f5a27d8fbb95fac397bdc8d
netchip <vanderpoel24@gmail.com>
2013-01-11 20:34:23 +0000

    XYAOSP

--- audio/Android.mk
+++ audio/Android.mk
@@ -28,6 +28,8 @@ LOCAL_C_INCLUDES += \
 	$(call include-path-for, audio-utils) \
 	$(call include-path-for, audio-effects)
 
+LOCAL_CFLAGS := -fno-strict-aliasing
+
 LOCAL_SHARED_LIBRARIES := liblog libcutils libtinyalsa libaudioutils libdl libexpat
 
 include $(BUILD_SHARED_LIBRARY)
